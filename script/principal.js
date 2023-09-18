20dad22be6a33e6bd0f811725fc1a35edb06fb39
vevekita <108936525+vevekita@users.noreply.github.com>
2023-09-18 11:44:21 -0300

    WINX

--- script/principal.js
+++ script/principal.js
@@ -20,4 +20,13 @@ eq2grau = (a,b,c) => {
 
 }
 
-escrever(eq2grau(1,3,-8));
+let a = "";
+let b = "";
+let op = "";
+let resultado = 0;
+
+function digitando (tecla){
+    a =+ tecla;
+    alert(tecla);
+
+}
